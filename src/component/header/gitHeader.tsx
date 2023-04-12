47cd0430729afa1c61f658c1254ebe17dd948b17
Apoles <emine176gumus@gmail.com>
2023-04-12 09:32:51 +0300

    invalid DOM property fix

--- src/component/header/gitHeader.tsx
+++ src/component/header/gitHeader.tsx
@@ -70,7 +70,7 @@ function Git() {
             viewBox='0 0 24 24'
             stroke='currentColor'
           >
-            <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16' />
           </svg>
         </button>
 
